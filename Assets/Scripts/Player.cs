ddafd509e789a222203b728d4726a68d2d7fc886
gelkoh <ole_kirchner@gmx.de>
2025-10-08 19:45:42 +0200

    Make camera follow player position

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,16 @@ public class Player : MonoBehaviour
     private InputAction m_moveAction;
     private InputAction m_lookAction;
 
+    private Camera m_mainCamera;
+    private Transform m_mainCameraTransform;
+
     void Start()
     {
         m_moveAction = InputSystem.actions.FindAction("Player/Move");
         m_lookAction = InputSystem.actions.FindAction("Player/Look");
+
+        m_mainCamera = Camera.main;
+        m_mainCameraTransform = m_mainCamera.GetComponent<Transform>();
     }
 
     void Update()
@@ -22,6 +28,9 @@ public class Player : MonoBehaviour
         float newX = currentX += (moveValue.x * 0.01f);
         float newY = currentY += (moveValue.y * 0.01f);
 
-        gameObject.transform.localPosition = new Vector3(newX, newY);
+        gameObject.transform.localPosition = new Vector3(newX, newY, 0);
+
+        // Update camera position to track player position
+        m_mainCameraTransform.position = new Vector3(newX, newY, -10);
     }
 }

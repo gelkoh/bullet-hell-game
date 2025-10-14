e9fea640fd9a058740d30f23f9985df2c32f0094
gelkoh <ole_kirchner@gmx.de>
2025-10-14 13:52:15 +0200

    Enemies now always look at player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,10 +5,27 @@ public class Enemy : MonoBehaviour {
     private FloatingHealthBar m_floatingHealthBar;
     private int m_currentHealth = 100;
     private int m_maximumHealth = 100;
+    private Camera m_mainCamera;
+    private Player m_player;
 
     void Awake()
     {
         m_floatingHealthBar = GetComponentInChildren<FloatingHealthBar>();
+        m_mainCamera = Camera.main;
+        m_player = Player.Instance;
+    }
+
+    void Update()
+    {
+        // TODO: This code is kind of repetitive to that in the player class, refactor later
+        Vector3 enemyScreenPosition = m_mainCamera.WorldToScreenPoint(gameObject.transform.localPosition);
+        Vector3 playerScreenPosition = m_mainCamera.WorldToScreenPoint(Player.Instance.gameObject.transform.localPosition);
+
+        float diffX = enemyScreenPosition.x - playerScreenPosition.x; 
+        float diffY = enemyScreenPosition.y - playerScreenPosition.y; 
+
+        float angle = -Mathf.Atan2(diffX, diffY) * Mathf.Rad2Deg;
+        gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 
     public void TakeDamage(int damage)

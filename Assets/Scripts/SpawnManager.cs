7cd3b889db59479c2507123f342f6b24bab682e1
gelkoh <ole_kirchner@gmx.de>
2025-10-19 15:25:39 +0200

    Enemies now spawn continuously

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
+using System.Collections;
 
 public class SpawnManager : MonoBehaviour
 {
     private Camera m_mainCamera;
-    
+
     [SerializeField]
     private GameObject m_enemyPrefab;
 
@@ -14,16 +15,25 @@ public class SpawnManager : MonoBehaviour
 
     void Start()
     {
-        Vector3 playerPosition = Player.Instance.transform.position;
-        float zDistanceToCamera = Mathf.Abs(m_mainCamera.transform.position.z - playerPosition.z);
-        Vector3 bottomLeft = m_mainCamera.ScreenToWorldPoint(new Vector3(0, 0, zDistanceToCamera));
-        float distanceToScreenCorner = Vector3.Distance(playerPosition, bottomLeft);
-        
-        float minimumSpawnDistance = distanceToScreenCorner + 1f;
-        float maximumSpawnDistance = distanceToScreenCorner + 5f;
-        
-        for (int i = 0; i < 10; i++)
+        StartCoroutine(SpawnRoutine(1));
+    }
+
+    private IEnumerator SpawnRoutine(float waitTime)
+    {
+        // TODO: Fix enemies spawning inside each other
+
+        while (true)
         {
+            yield return new WaitForSeconds(waitTime);
+
+            Vector3 playerPosition = Player.Instance.transform.position;
+            float zDistanceToCamera = Mathf.Abs(m_mainCamera.transform.position.z - playerPosition.z);
+            Vector3 bottomLeft = m_mainCamera.ScreenToWorldPoint(new Vector3(0, 0, zDistanceToCamera));
+            float distanceToScreenCorner = Vector3.Distance(playerPosition, bottomLeft);
+
+            float minimumSpawnDistance = distanceToScreenCorner + 1f;
+            float maximumSpawnDistance = distanceToScreenCorner + 5f;
+
             int randomNumber = Random.Range(0, 359);
             Vector2 direction = new Vector2(Mathf.Cos(randomNumber), Mathf.Sin(randomNumber)).normalized;
 

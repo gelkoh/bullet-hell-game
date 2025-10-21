324459d9d4eafb853eed12b55d2f78817e25da22
gelkoh <ole_kirchner@gmx.de>
2025-10-21 11:55:42 +0200

    Master volume is now stored persistently in PlayerPrefs

--- Assets/Scripts/MasterVolumeSlider.cs
+++ Assets/Scripts/MasterVolumeSlider.cs
@@ -7,9 +7,29 @@ public class MasterVolumeSlider : MonoBehaviour
     [SerializeField]
     private AudioMixer m_masterMixer;
 
-    public void HandleVolumeChange(float volume)
+    private Slider m_slider;
+
+    private const string VOLUME_KEY = "MasterVolumeLinear";
+    private const float DEFAULT_VOLUME = 1f;
+
+    void Awake()
+    {
+        m_slider = GetComponent<Slider>();
+
+        float linearVolume = PlayerPrefs.GetFloat(VOLUME_KEY, DEFAULT_VOLUME);
+        m_slider.value = linearVolume;
+
+        // In case m_slider.value doesn't trigger OnValueChanged event
+        HandleVolumeChanged(linearVolume);
+    }
+
+    public void HandleVolumeChanged(float volume)
     {
         float decibels = Mathf.Log10(volume) * 20f;
+
         m_masterMixer.SetFloat("MasterVolume", decibels);
+
+        PlayerPrefs.SetFloat(VOLUME_KEY, volume);
+        PlayerPrefs.Save();
     }
 }
